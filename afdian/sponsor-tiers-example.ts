917bc9edcbbc7d8b08ddc87200798bc537281b3c
Gizmo <me@aika.dev>
2024-07-18 09:18:09 +0800

    chore: update sponsor-tiers-example

--- afdian/sponsor-tiers-example.ts
+++ afdian/sponsor-tiers-example.ts
@@ -10,8 +10,9 @@ export function tiers(sponsors: SponsorDetails[]): SponsorTier[] {
     {
       title: 'Gold Sponsors',
       badge: {
-        avatarSize: 90,
-        showName: true
+        avatarSize: 70,
+        showName: true,
+        padding: 30
       },
       sort: 0,
       r(current: SponsorDetails) {
@@ -24,8 +25,9 @@ export function tiers(sponsors: SponsorDetails[]): SponsorTier[] {
     {
       title: 'Silver Sponsors',
       badge: {
-        avatarSize: 70,
-        showName: true
+        avatarSize: 60,
+        showName: true,
+        padding: 30
       },
       sort: 1,
       r(current: SponsorDetails) {
@@ -38,34 +40,42 @@ export function tiers(sponsors: SponsorDetails[]): SponsorTier[] {
     {
       title: 'Sponsors',
       badge: {
-        avatarSize: 50
+        avatarSize: 55,
+        showName: true,
+        padding: 30
       },
       sort: 2,
       r(current: SponsorDetails) {
         if (check([0, 1], all, current)) return false;
-        return parseFloat(current.all_sum_amount) >= 100;
+        if (current.current_plan.name === '') return false;
+        return parseFloat(current.all_sum_amount) >= 5;
       }
     },
     {
       title: 'Backers',
       badge: {
-        avatarSize: 40
+        avatarSize: 45,
+        showName: true,
+        padding: 24
       },
       sort: 3,
       r(current: SponsorDetails) {
-        if (check([0, 1], all, current)) return false;
-        const price = parseFloat(
-          current.current_plan.show_price ?? current.current_plan.price
-        );
-        return price >= 15;
+        if (check([0, 1, 2], all, current)) return false;
+        const price = parseFloat(current.all_sum_amount);
+        return price >= 5;
       }
     }
   ];
-  for (const t of all) t.sponsors = sponsors.filter(s => t.r(s));
-  return all.sort((a, b) => a.sort - b.sort);
+  return all
+    .filter(t => {
+      t.sponsors = sponsors.filter(s => t.r(s));
+      return t.sponsors && t.sponsors.length > 0;
+    })
+    .sort((a, b) => a.sort - b.sort);
 }
 
 function check(indexs: number[], all: Tier[], current: SponsorDetails) {
+  if (current.user.user_id === '75e549844b5111ed8df552540025c377') return true;
   return indexs
     .map(i => {
       return all[i].sponsors
