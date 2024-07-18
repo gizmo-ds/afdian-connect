eb815b4de2270920b8d881c6e108058c1902c3d1
Gizmo <me@aika.dev>
2024-07-18 09:18:35 +0800

    feat: add AFDIAN_URL_BASE

--- afdian/api.ts
+++ afdian/api.ts
@@ -1,18 +1,17 @@
+import { AFDIAN_URL_BASE } from '../utils/secret';
 import { request } from './helpers';
 import { GetProfileResult, QueryOrderResult, SponsorResult } from './types';
 
 export async function queryOrder(page: number = 1, orders?: string[] | string) {
-  return await request<QueryOrderResult>(
-    'https://afdian.net/api/open/query-order',
-    {
-      page,
-      out_trade_no: orders
-        ? Array.isArray(orders)
-          ? orders.join(',')
-          : orders
-        : undefined
-    }
-  );
+  const u = new URL('/api/open/query-order', AFDIAN_URL_BASE);
+  return await request<QueryOrderResult>(u.toString(), {
+    page,
+    out_trade_no: orders
+      ? Array.isArray(orders)
+        ? orders.join(',')
+        : orders
+      : undefined
+  });
 }
 
 export async function querySponsor(
@@ -20,22 +19,20 @@ export async function querySponsor(
   per_page: number = 100,
   user_ids?: string[] | string
 ) {
-  return await request<SponsorResult>(
-    'https://afdian.net/api/open/query-sponsor',
-    {
-      page,
-      per_page,
-      user_id: user_ids
-        ? Array.isArray(user_ids)
-          ? user_ids.join(',')
-          : user_ids
-        : undefined
-    }
-  );
+  const u = new URL('/api/open/query-sponsor', AFDIAN_URL_BASE);
+  return await request<SponsorResult>(u.toString(), {
+    page,
+    per_page,
+    user_id: user_ids
+      ? Array.isArray(user_ids)
+        ? user_ids.join(',')
+        : user_ids
+      : undefined
+  });
 }
 
 export async function getProfileBySlug(slug: string) {
-  const u = new URL('https://afdian.net/api/user/get-profile-by-slug');
+  const u = new URL('/api/user/get-profile-by-slug', AFDIAN_URL_BASE);
   u.searchParams.append('url_slug', slug);
   return (await fetch(u.toString()).then(resp =>
     resp.json()
@@ -43,7 +40,7 @@ export async function getProfileBySlug(slug: string) {
 }
 
 export async function getProfile(user_id: string) {
-  const u = new URL('https://afdian.net/api/user/get-profile');
+  const u = new URL('/api/user/get-profile', AFDIAN_URL_BASE);
   u.searchParams.append('user_id', user_id);
   return (await fetch(u.toString()).then(resp =>
     resp.json()
--- utils/secret.ts
+++ utils/secret.ts
@@ -3,3 +3,5 @@ export const AFDIAN_USER_ID = process.env.AFDIAN_USER_ID;
 export const AFDIAN_USER = process.env.AFDIAN_USER;
 export const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 export const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+export const AFDIAN_URL_BASE =
+  process.env.AFDIAN_URL_BASE || 'https://afdian.com';
